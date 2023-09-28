302c79c597258ad1d0eed0cf50967638643fea93
rishi <119162955+RishithReddyRR@users.noreply.github.com>
2023-09-28 06:59:02 +0530

    Update jwtTokens.js

--- utils/jwtTokens.js
+++ utils/jwtTokens.js
@@ -8,8 +8,7 @@ const sendToken = (user, statusCode, res) => {
       expires: new Date(
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
-      httpOnly: true,
-      domain: 'https://konukkupoo-frontend-py93w25vu-rishithreddyrr.vercel.app/',
+      domain: 'vercel.app/',
         secure: true 
     };
   
