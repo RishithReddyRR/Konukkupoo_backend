e33f9cde81bc78fb627ccc1fc2aa9e2fe0177c7d
rishi <119162955+RishithReddyRR@users.noreply.github.com>
2023-09-28 07:04:53 +0530

    Update jwtTokens.js

--- utils/jwtTokens.js
+++ utils/jwtTokens.js
@@ -8,7 +8,8 @@ const sendToken = (user, statusCode, res) => {
       expires: new Date(
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
-      domain: 'vercel.app/',
+      domain: '.vercel.app',
+        httpOnly:true,
         secure: true 
     };
   
